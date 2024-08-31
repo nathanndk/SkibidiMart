c102b4e500cc9630cda467106c61ffde47006b3f
christianjoshua <Razor091203@gmail.com>
2024-08-31 12:05:09 +0700

    feat: add sign-in form interface

---
+++ src/app/(admin)/dashboard/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import FormSignIn from "./_components/form";
+
+export default function SignInPage() {
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <FormSignIn></FormSignIn>
+    </main>
+  );
+}
